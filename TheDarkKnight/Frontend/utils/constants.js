08d703cde7ed55144d39440c7b700bc3abc86d1d
PeterParker303e24 <peterparker303e24@proton.me>
2026-07-27 03:11:47 +0000

    Updated single hash constants for local and remote blockchains.

--- TheDarkKnight/Frontend/utils/constants.js
+++ TheDarkKnight/Frontend/utils/constants.js
@@ -223,7 +223,21 @@ export const CHAIN_NAME
     = chainName;
 
 // Single hash requirement data
-export const SINGLE_HASH_REQUIREMENT_HASH
-    = "0x584bc4c338df9c2bfd77a440ca0f152b9075eaaac10e7c2174ab6213b3e67b58";
-export const SINGLE_HASH_REQUIREMENT_INDEX = "1";
-export const SINGLE_HASH_REQUIREMENT_VERSION_INDEX = "1";
+let singleHashRequirementHash;
+let singleHashRequirementIndex;
+let singleHashRequirementVersionIndex;
+if (getCookie("isLocalBlockchain") === "true") {
+    singleHashRequirementHash
+        = "0x584bc4c338df9c2bfd77a440ca0f152b9075eaaac10e7c2174ab6213b3e67b58";
+    singleHashRequirementIndex = 1;
+    singleHashRequirementVersionIndex = 1;
+} else {
+    singleHashRequirementHash
+        = "0x584bc4c338df9c2bfd77a440ca0f152b9075eaaac10e7c2174ab6213b3e67b58";
+    singleHashRequirementIndex = 1;
+    singleHashRequirementVersionIndex = 2;
+}
+export const SINGLE_HASH_REQUIREMENT_HASH = singleHashRequirementHash;
+export const SINGLE_HASH_REQUIREMENT_INDEX = singleHashRequirementIndex;
+export const SINGLE_HASH_REQUIREMENT_VERSION_INDEX
+    = singleHashRequirementVersionIndex;
